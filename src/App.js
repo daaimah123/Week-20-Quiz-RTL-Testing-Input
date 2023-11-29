8f3eb2c8562cece9021f4d7f587e334134f14810
Daaimah Tibrey <daaimahtibrey@Daaimahs-Laptop.hsd1.ca.comcast.net>
2023-11-29 12:31:08 -0800

    remove Scenario description property

--- src/App.js
+++ src/App.js
@@ -19,24 +19,11 @@ function App() {
 
             <Scenario
                 component={<ChangeInput />}
-                description={
-                    <ul>
-                        <li>Use <code>getByLabelText</code> to select elements via <code>aria-label</code>.</li>
-                        <li>Use <code>fireEvent.change</code> to simulate change events. Accepts an <code>event</code> object.</li>
-                    </ul>
-                }
                 title="Testing for an Input Change"
             />
 
             <Scenario
                 component={<FocusInput />}
-                description={
-                    <ul>
-                        <li>Use <code>container.firstChild</code> is the DOM tree of the component and can be used in snapshot testing.</li>
-                        <li>Use <code>getByPlaceholderText</code> to select via input <code>placeholder</code>.</li>
-                        <li>Use <code>document.activeElement</code> is the same DOM node returned by the getter.</li>
-                    </ul>
-                }
                 title="Testing a Focused Element"
             />
 
